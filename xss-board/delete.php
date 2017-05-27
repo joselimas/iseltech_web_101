4fe05bd8180e6af16e55462f997a769337c2cb99
joselimas <jose.limas@gmail.com>
2017-05-27 03:30:07 +0100

    xss trips

--- xss-board/delete.php
+++ xss-board/delete.php
@@ -1,16 +1,10 @@
 <?php
-    if($_COOKIE["PHPSESSID"]=='mag1c_c00k1e'){
+    if (isset($_COOKIE["PHPSESSID"])) {
         $files = glob('/var/www/public/comments/*'); // get all file names
         foreach($files as $file){ // iterate files
           if(is_file($file))
-            unlink($file); // delete file
-        }
-    }elseif (isset($_COOKIE["PHPSESSID"])) {
-        $files = glob('/var/www/public/comments/*'); // get all file names
-        foreach($files as $file){ // iterate files
-          if(is_file($file))
-            $content = file("./comments/$file");
-            if ($content[0]==$_COOKIE["PHPSESSID"]){
+            $content = file($file);
+            if ($content[0]==$_COOKIE["PHPSESSID"]."\n" || $_COOKIE["PHPSESSID"]=='mag1c_c00k1e'){
                 unlink($file); // delete file
             }
         }
